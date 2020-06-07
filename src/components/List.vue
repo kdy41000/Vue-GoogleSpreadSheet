01a0bd66c5a7df0116c509d0d80684831eafaf34
kdy41000 <kdy41000@naver.com>
2020-06-07 14:10:49 +0900

    connect database2

---
+++ src/components/List.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>리스트</h1>
+    </div>    
+</template>
+<script>
+export default {
+    name : 'list',
+    beforeMount() {
+         this.$axios({
+             url:'/selectlist',
+             method:'GET'
+         })
+         .then((response) => {
+             alert(response.data);
+         })
+         .catch(() => {
+             alert("에러");
+         })
+    }
+}
+</script>
